5c0ca42c63f686666558f99a83bec3e4a14080a5
Igor_U <fixtor75@gmail.com>
2022-09-03 20:09:31 +0300

    Задача 66 Готова

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -1,18 +1,21 @@
 ﻿// 64. Задайте значение M и N. Напишите программу, 
 //которая выведет все натуральные числа в промежутке от M до N.
 
+// 64. Задайте значение M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
 Console.Clear();
 Console.Write("Введите M: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите N: ");
 int n = int.Parse(Console.ReadLine());
 
-PrintNumbers(m,n);
+PrintNumbers(m, n);
 
 void PrintNumbers(int start, int end)
 {
     Console.Write($"{start} ");
-    
+
     if (start == end) return;
     PrintNumbers(start + 1, end);
 }
