c648751ad8316b8acda41d248e47ee6af9965418
Igor_U <fixtor75@gmail.com>
2022-09-03 19:19:33 +0300

    Задача 64 Готова

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 64. Задайте значение M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(m,n);
+
+void PrintNumbers(int start, int end)
+{
+    Console.Write($"{start} ");
+    
+    if (start == end) return;
+    PrintNumbers(start + 1, end);
+}
