e2f5be1613054686ababefd0d696a75b40737fd1
Igor_U <fixtor75@gmail.com>
2022-09-03 20:16:02 +0300

    Задача 64 Готова

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -7,8 +7,10 @@ Console.Write("Введите M: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите N: ");
 int n = int.Parse(Console.ReadLine());
-
-PrintNumbers(m, n);
+if (m > n)
+    System.Console.WriteLine("Вы ввели неверное чиcло!");
+else
+    PrintNumbers(m, n);
 
 void PrintNumbers(int start, int end)
 {
