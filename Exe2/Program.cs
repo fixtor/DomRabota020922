2d87aacc35807bc094f93e1547c83bdd45afead8
Igor_U <fixtor75@gmail.com>
2022-09-03 20:09:31 +0300

    Задача 66 Готова

---
+++ Exe2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summ = GetSumm(m, n);
+System.Console.WriteLine(summ);
+
+
+int GetSumm(int start, int end)
+{
+    if (start == end) return start;
+
+    return end + GetSumm(start, end - 1);
+}
